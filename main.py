2c5f5bf6e784c8bcb78457e719af503cfc07a258
GiuliaPeretti <giulia.peretti12@gmail.com>
2024-06-21 12:36:47 +0200

    methot di update

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from settings import *
 import ast
 
 def draw_background():
-    screen.fill((240,240,240))
+    screen.fill(BACKGROUND_COLOR)
     draw_bars()
     draw_text()
     display_progress()
@@ -57,7 +57,19 @@ def draw_buttons():
 
     return(b_set_goal,b_add_hours,b_add_ticket)
 
+def clear_progres():
+    bar_width=(SCREEN_WIDTH-100)
+    x1,y1,w,h=50, (SCREEN_HEIGHT/4), bar_width, 30
+    pygame.draw.rect(screen, BACKGROUND_COLOR, (x1,y1,w,h))
+
+    bar_width=(SCREEN_WIDTH-100)
+    x1,y1,w,h=50, 1.8*(SCREEN_HEIGHT/4), bar_width, 30
+    pygame.draw.rect(screen, BACKGROUND_COLOR, (x1,y1,w,h))
+    draw_bars()
+
+
 def display_progress():
+    clear_progres()
     color_hours=[(255,0,0),(255,100,0),(255,255,0),(150,255,0),(0,255,0),(0,255,150),(0,255,255),(0,150,255),(0,0,255),(150,0,255),(255,0,255),(255,0,150)]
     with open('data.json', 'r') as f:
         l=f.read()
@@ -75,13 +87,55 @@ def display_progress():
     y1=SCREEN_HEIGHT/4
     for i in range(len(hours_sessions)):
         bar_width=int((SCREEN_WIDTH-100)*(int(hours_sessions[i])/hours_goal))
-        print(bar_width)
         pygame.draw.rect(screen, color_hours[i%len(color_hours)], (x1,y1,bar_width,h))
         x1=x1+bar_width
-        print(x1)
     draw_bars()
     #TODO: barra colorata ticket
 
+def set_ticket_goal(n):
+    if(n>0):
+        with open('data.json', 'r') as f:
+            l=f.read()
+        l=ast.literal_eval(l)
+        l['ticket_goal']=n
+        l=str(l)
+        l=l.replace("'",'"')
+        with open('data.json', 'w') as f:
+            f.write(l)
+        print("goal setted at"+str(n))
+
+def add_ticket(n):
+    if(n>=0):
+        with open('data.json', 'r') as f:
+            l=f.read()
+        l=ast.literal_eval(l)
+        if(l['current_ticket']+n>=l['ticket_goal']):
+            l['current_ticket']=l['ticket_goal']
+        else:
+            l['current_ticket']=l['current_ticket']+n
+        l=str(l)
+        l=l.replace("'",'"')
+        with open('data.json', 'w') as f:
+            f.write(l)
+        print("added ticket "+str(n))
+
+
+def add_ticket(n):
+    if(n>=0):
+        with open('data.json', 'r') as f:
+            l=f.read()
+        l=ast.literal_eval(l)
+        #sommare tutti gli elementi dell'array e controllare che non sia maggiore del goal
+        #se si append la differenza o 0
+        if(l['hours_sessions']+n>=l['hours_goal']):
+            l['hours_sessions']=l['hours_sessions'].append()
+        else:
+            l['hours_sessions']=l['hours_sessions'].append(n)
+        l=str(l)
+        l=l.replace("'",'"')
+        with open('data.json', 'w') as f:
+            f.write(l)
+        print("added ticket "+str(n))
 
 
 
@@ -94,6 +148,7 @@ run  = True
 selected_cell=(-1,-1)
 draw_background()
 b_set_goal,b_add_hours,b_add_ticket = draw_buttons()
+number=0
 while run:
 
     for event in pygame.event.get():
@@ -103,10 +158,26 @@ while run:
             x,y=pygame.mouse.get_pos()
             if(x>b_set_goal[0][0] and x<=b_set_goal[0][1] and y>=b_set_goal[1][0] and y<=b_set_goal[1][1]):
                 print("set goal")
+                set_ticket_goal(number)
+                display_progress()
+                number=0
             if(x>b_add_hours[0][0] and x<=b_add_hours[0][1] and y>=b_add_hours[1][0] and y<=b_add_hours[1][1]):
                 print("add hours")
             if(x>b_add_ticket[0][0] and x<=b_add_ticket[0][1] and y>=b_add_ticket[1][0] and y<=b_add_ticket[1][1]):
                 print("add ticket")
+                add_ticket(number)
+                display_progress()
+                number=0
+        if (event.type == pygame.KEYDOWN):
+            if event.key==pygame.K_SPACE:
+                set_ticket_goal(number)
+            for i in range(len(INPUTS)):
+                if (event.key==INPUTS[i]):
+                    number=number*10+i
+                    print(number)
+
+
+
     pygame.display.flip()
     clock.tick(30)
     
--- settings.py
+++ settings.py
@@ -1,6 +1,7 @@
 import pygame
 
 SCREEN_WIDTH,SCREEN_HEIGHT = 600,400
+BACKGROUND_COLOR = (240,240,240)
 
 flags=(pygame.HWSURFACE | pygame.DOUBLEBUF)
-INPUTS=[pygame.K_1,pygame.K_2,pygame.K_3,pygame.K_4,pygame.K_5,pygame.K_6,pygame.K_7,pygame.K_8,pygame.K_9]
+INPUTS=[pygame.K_0,pygame.K_1,pygame.K_2,pygame.K_3,pygame.K_4,pygame.K_5,pygame.K_6,pygame.K_7,pygame.K_8,pygame.K_9]
